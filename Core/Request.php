4c6f2f55a8a0b00ce5ad148aa66147b16f6636a2
Quentin <contact@ypio.fr>
2019-04-06 22:35:13 +0200

    trigger_error instead of return when trying to access undefined index in GET or POST

--- Core/Request.php
+++ Core/Request.php
@@ -2,27 +2,47 @@
 
 namespace Core;
 
-use mysql_xdevapi\Exception;
-
+/**
+ * Class Request
+ * @package Core
+ */
 class Request
 {
 
+    /**
+     * @var array
+     */
     private $get = array();
 
+    /**
+     * @var array
+     */
     private $post = array();
 
+    /**
+     * @var
+     */
     private static $_instance;
 
+    /**
+     * @param $get
+     */
     public function setGet($get)
     {
         $this->get = $get;
     }
 
+    /**
+     * @param $post
+     */
     public function setPost($post)
     {
         $this->post = $post;
     }
 
+    /**
+     * @return Request
+     */
     public static function getInstance(): Request
     {
         if (is_null(self::$_instance)) {
@@ -31,21 +51,30 @@ class Request
         return self::$_instance;
     }
 
+    /**
+     * @param $var
+     * @return mixed|string
+     */
     public function get($var)
     {
-        if(isset($this->get[$var])){
+        if (isset($this->get[$var])) {
             return $this->get[$var];
         }
         $debug = debug_backtrace();
-        return "<p>Framework warning: Undefined index $var in GET " . $debug[0]['file'] . " at line " . $debug[0]['line'] . '</p>';
+        trigger_error("<p>Framework warning: Undefined index $var in Request->get " . $debug[0]['file'] . " at line " . $debug[0]['line'] . '</p>');
     }
 
+    /**
+     * @param $var
+     * @return mixed|string
+     */
     public function post($var)
     {
-        if(isset($this->post[$var])){
+        if (isset($this->post[$var])) {
             return $this->post[$var];
         }
         $debug = debug_backtrace();
-        return "<p>Framework warning: Undefined index $var in POST " . $debug[0]['file'] . " at line " . $debug[0]['line'] . '</p>';    }
+        trigger_error("<p>Framework warning: Undefined index $var in Request->post " . $debug[0]['file'] . " at line " . $debug[0]['line'] . '</p>');
+    }
 
 }
